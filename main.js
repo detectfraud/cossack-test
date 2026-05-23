b0da845fce58f3cc1b3f8cf98ecb0fb9e6e59353
detectfraud <vib10@ukr.net>
2026-05-23 10:37:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ const I18N = {
 // Глобальні прапорці безпеки
 window._isAdblockDetected = false;
 const keyTimeHash = "u_data_ts";      
-/*const keyStringHash = "u_data_str"; */ 
+const keyStringHash = "u_data_str";  
 
 // Функції інкогніто-маскування LocalStorage
 function maskData(value) {

c3749f21570bc5304917994fd0276a4a9eebdad8
detectfraud <vib10@ukr.net>
2026-05-28 13:28:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
    КОЗАЦЬКИЙ СЕРІАЛ — main.js 
    ============================================= */
 
-const SMARTLINK = "https://raw.githubusercontent.com/detectfraud/cossack-test/main/Kozaki.png";
+const SMARTLINK = "https://www.effectivecpmnetwork.com/h0798697?key=ad572362eed5bbf9c60c6dc19f92f878";
 
 const POST_CONFIG = {
   likes:    "109K",
